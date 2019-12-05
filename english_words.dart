b9186c8d4129ad627d9d12f5fa2c32e7d012a3fa
Florian Müller <contact@flomllr.com>
2019-12-05 14:02:28 +0100

    Add replacement for 'english_words.dart'
    Add replacement for 'package:english_words/english_words.dart'

---
+++ english_words.dart
@@ -0,0 +1,152 @@
+// Copy the following imports to the top of your file
+import 'dart:collection';
+import 'dart:math';
+
+// Copy the following code to the end of your file in dartpad.dev
+class WordPair {
+  String word1;
+  String word2;
+  String asPascalCase;
+  
+  WordPair(this.word1, this.word2) {
+    this.asPascalCase = 
+      this.word1.toLowerCase() + 
+      this.word2[0].toUpperCase() + 
+      this.word2.substring(1).toLowerCase();
+  }
+}
+
+class RandomWordPairIterator extends Iterator<WordPair> {
+  WordPair current;
+  final _random = new Random();
+  bool moveNext() {
+    final index = _random.nextInt(50);
+    final word1 = mostCommonAdjectives[index];
+    final word2 = mostCommonNouns[index];
+    this.current = WordPair(word1, word2);
+    return true;
+  }
+}
+
+class RandomWordPairIterable extends IterableMixin<WordPair> {
+  final Iterator<WordPair> iterator;
+  RandomWordPairIterable(this.iterator);
+}
+
+Iterable<WordPair> generateWordPairs() {
+  final iterator = new RandomWordPairIterator();
+  return RandomWordPairIterable(iterator);
+}
+
+List<String> mostCommonAdjectives = [
+  'able',
+  'bad',
+  'best',
+  'better',
+  'big',
+  'black',
+  'certain',
+  'clear',
+  'different',
+  'early',
+  'easy',
+  'economic',
+  'federal',
+  'free',
+  'full',
+  'good',
+  'great',
+  'hard',
+  'high',
+  'human',
+  'important',
+  'international',
+  'large',
+  'late',
+  'little',
+  'local',
+  'long',
+  'low',
+  'major',
+  'military',
+  'national',
+  'new',
+  'old',
+  'only',
+  'other',
+  'political',
+  'possible',
+  'public',
+  'real',
+  'recent',
+  'right',
+  'small',
+  'social',
+  'special',
+  'strong',
+  'sure',
+  'true',
+  'white',
+  'whole',
+  'young',
+];
+
+List<String> mostCommonNouns = [
+    'area',
+  'book',
+  'business',
+  'case',
+  'child',
+  'company',
+  'country',
+  'day',
+  'eye',
+  'fact',
+  'family',
+  'government',
+  'group',
+  'hand',
+  'home',
+  'job',
+  'life',
+  'lot',
+  'man',
+  'money',
+  'month',
+  'mother',
+  'Mr',
+  'night',
+  'number',
+  'part',
+  'people',
+  'place',
+  'point',
+  'problem',
+  'program',
+  'question',
+  'right',
+  'room',
+  'school',
+  'state',
+  'story',
+  'student',
+  'study',
+  'system',
+  'thing',
+  'time',
+  'water',
+  'way',
+  'week',
+  'woman',
+  'word',
+  'work',
+  'world',
+  'year',
+];
+
+
+
+
+
+
+
